db0f0929bbc9b349b636e7acc567dba0e0834ed9
Lee Xiang Wei <xwlee2009@gmail.com>
2020-04-27 00:00:46 +0800

    Initial commit

---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+
+namespace OdeToFood.Pages.Restaurants
+{
+    public class ListModel : PageModel
+    {
+        private readonly IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
